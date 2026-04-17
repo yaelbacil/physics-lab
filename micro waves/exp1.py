b2fab46cc5a9b7e1dc21810f9e4726de70737a91
Yael Bacil <68613243+yaelbacil@users.noreply.github.com>
2026-04-17 13:55:53 +0300

    updating the code to be relevant only for experiment 1 analsys
    Removed unused functions and data loading for experiments 3 and 4.

--- micro waves/exp1.py
+++ micro waves/exp1.py
@@ -4,32 +4,10 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import scipy.stats as stats
 
-def get_experiment_data(df, start_col, end_col):
-  # the function gets the relevant coloumns
-  # returns specific sub_experiment data from the csv file 'data micro'
-  exp_df = df.iloc[1:, start_col:end_col].copy()
-  exp_df.columns = exp_df.iloc[0]
-  exp_df = exp_df.iloc[1:].dropna(how='all').reset_index(drop=True)
-  return exp_df
-
 # calling data from github reposatory
-# m1 - meusrment number 1
 exp1 = "https://raw.githubusercontent.com/yaelbacil/physics-lab/refs/heads/main/exp1.csv"
-exp3 = 'https://raw.githubusercontent.com/yaelbacil/physics-lab/refs/heads/main/exp%203%20mesure%201.csv'
-exp4_m1 = 'https://raw.githubusercontent.com/yaelbacil/physics-lab/refs/heads/main/exp%204%20mesure%201.csv'
-exp4_m2 = 'https://raw.githubusercontent.com/yaelbacil/physics-lab/refs/heads/main/exp%204%20mesure%202.csv'
-micro_data_path = "https://raw.githubusercontent.com/yaelbacil/physics-lab/refs/heads/main/data%20micro.csv"
-
-# creating dataframes
 df_exp1 = pd.read_csv(exp1)
-df_exp3 = pd.read_csv(exp3)
-df_exp4_m1 = pd.read_csv(exp4_m1)
-df_exp4_m2 = pd.read_csv(exp4_m2)
-df_full = pd.read_csv(micro_data_path)
 
-# calling subexperiments from 'data micro' file
-df_exp2 = get_experiment_data(df_full, 4, 7)
-df_exp5 = get_experiment_data(df_full, 8, 10)
 # fit and plot for exp1
 
 def cos_offset_model(theta_rad, A, B):
